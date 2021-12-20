b0d41d54bed5edc0551629cbaceb7c96f9a0d317
niczow <61888973+niczow@users.noreply.github.com>
2021-12-20 18:45:23 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.1.0'
+PACKAGE_NAME = 'ninput'
+AUTHOR = 'Kacper Nowicz'
+AUTHOR_EMAIL = 'niczow@op.pl'
+URL = 'https://github.com/niczow/ninput'
+
+LICENSE = 'Apache License 2.0'
+DESCRIPTION = 'Gives option to input numbers easier'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'os'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
