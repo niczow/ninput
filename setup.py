b9a4c244bfed0557cc5b05e71a6430db3aff7fa6
niczow <61888973+niczow@users.noreply.github.com>
2021-12-21 01:16:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.1.1'
+VERSION = '0.1.3'
 PACKAGE_NAME = 'ninput'
 AUTHOR = 'Kacper Nowicz'
 AUTHOR_EMAIL = 'niczow@op.pl'
